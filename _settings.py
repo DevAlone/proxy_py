7662eac067510bdc36e3554351ec100f5bbe5eff
DevAlone <admin@d3d.info>
2017-09-15 13:06:38 +0300

    fix

--- _settings.py
+++ _settings.py
@@ -9,14 +9,14 @@ ALLOWED_HOSTS = ['localhost']
 
 DATABASES = {
     'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'proxy_py',
-#        'USER': 'proxy_py',
-#        'PASSWORD': 'proxy_py',
-#        'HOST': '127.0.0.1',
-#        'PORT': '5432',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'proxy_py',
+        'USER': 'proxy_py',
+        'PASSWORD': 'proxy_py',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
 }
 
