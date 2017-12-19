b470f0ed7d60f2f8755102f9e2955a9984d37d31
DevAlone <admin@d3d.info>
2017-12-19 23:34:50 +0300

    remove django

--- init_django.py
+++
@@ -1,5 +0,0 @@
-import django
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proxy_py.settings")
-django.setup()
--- proxy_py/_settings.py
+++ proxy_py/_settings.py
@@ -8,44 +8,7 @@ DATABASE_CONNECTION_KWARGS = {}
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# change it before deploying
-# SECRET_KEY = 'sadfsadfkjl324h5kl23jklj231$@!#$SadfgasdjkfhJKHSJLKADH7234@#',
-
-# ALLOWED_HOSTS = []  # ['localhost']
 DEBUG = True
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     },
-# }
-
-# INSTALLED_APPS = [
-#     "core.apps.CoreConfig",
-# ]
-
-# ROOT_URLCONF = "proxy_py.urls"
-
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
-#
-
-# WSGI_APPLICATION = 'proxy_py.wsgi.application'
-# STATIC_URL = '/static/'
 
 # fetcher settings
 
