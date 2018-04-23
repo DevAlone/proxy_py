cb05f4eb68ba74fc04e3502db03a6e2f13c9ee19
DevAlone <admin@d3d.info>
2018-04-24 00:48:24 +0300

    fix

--- processor.py
+++ processor.py
@@ -89,7 +89,6 @@ class Processor:
 
     async def process_collectors(self):
         while True:
-            print('checking collectors')
             await asyncio.sleep(0.01)
             try:
                 # TODO: split checking collectors and proxies to the different coroutines
@@ -117,7 +116,6 @@ class Processor:
 
     async def process_proxies(self):
         while True:
-            print('checking proxies')
             await asyncio.sleep(0.01)
             try:
                 # check good proxies
